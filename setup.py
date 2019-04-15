1ca987b9769e8f661b654f434ce44827bc70ac76
TomerFi <tomer.figenblat@gmail.com>
2019-04-15 20:40:07 +0300

    Updated classifiers.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="aioswitcher",
     version="2019.3.21",
     author="Tomer Figenblat",
@@ -12,14 +12,20 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tomerfi/aioswitcher",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
         "Framework :: AsyncIO",
         "Framework :: Flake8",
-        "Intended Audience :: Developers"
+        "Framework :: Pytest",
+        "Framework :: tox",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Home Automation",
+        "Typing :: Typed"
     ]
 )
