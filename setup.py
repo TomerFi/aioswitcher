56cb4c29d66857274d154de2eb70b95284da55c1
TomerFi <tomer.figenblat@gmail.com>
2019-04-17 00:08:03 +0300

    Added python3.6 classifier.

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Home Automation",
