1f27a47f46b04e59ba814077a5e92ca2c1a0170c
TomerFi <tomer.figenblat@gmail.com>
2019-06-03 16:14:26 +0300

    Added bandit code security tests.

--- tests/common.py
+++ tests/common.py
@@ -13,8 +13,8 @@ def create_random_time(hour_delta: int = 1) -> str:
     """Use to create a random HH:MM time."""
     min_hour = (datetime.now() + timedelta(hours=hour_delta)).hour
     return '{}:{}'.format(
-        '00{}'.format(str(randint(min_hour, 23)))[-2:],
-        '00{}'.format(str(randint(0, 59)))[-2:])
+        '00{}'.format(str(randint(min_hour, 23)))[-2:],  # nosec
+        '00{}'.format(str(randint(0, 59)))[-2:])  # nosec
 
 
 def get_later_time_for_minute_delta(minute_delta: int = 30) -> str:
