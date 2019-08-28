b90e3306c269c21eaebd0f7978b3220c2859bd79
Tomer Figenblat <tomer.figenblat@gmail.com>
2019-08-28 10:56:52 +0300

    Removed transitive dependencies from requirements file.

--- pyscripts/poetry-to-requirements.py
+++ pyscripts/poetry-to-requirements.py
@@ -1,19 +1,33 @@
 """Python script for creating requirements.txt file.
 
 Python script for converting poetry dependencies from
-poetry.lock to requirements.txt for legacy support.
+pyproject.toml to requirements.txt for legacy support.
 """
 
 from toml import load as toml_load
 
 if __name__ == "__main__":
-    parsed_toml = toml_load("poetry.lock")
+    parsed_toml = toml_load("pyproject.toml")
     with open("requirements.txt", "w+") as req_file:
         req_file.write(
             "# This file exists for legacy support. "
             + "Do not use for requirements instllations.\r\n"
         )
-        for pkg in parsed_toml["package"]:
-            req_file.write("{}=={}\r\n".format(pkg["name"], pkg["version"]))
+        for dep in parsed_toml["tool"]["poetry"]["dependencies"]:
+            if dep.lower() != "python":
+                if (
+                    "version"
+                    in parsed_toml["tool"]["poetry"]["dependencies"][dep]
+                ):
+                    req_file.write(
+                        "{}=={}\r".format(
+                            dep,
+                            parsed_toml["tool"]["poetry"]["dependencies"][dep][
+                                "version"
+                            ],
+                        )
+                    )
+                else:
+                    req_file.write("{}\r".format(dep))
 
     print("requirements.txt was updated, please push to scm.")
