f0d96bb931a591f0df5eecaad8ce802ee378034e
Tomer Figenblat <tomer.figenblat@gmail.com>
2021-06-04 10:41:19 +0300

    refactor: started utils script to better accommodate the library its predecessor tools script

---
+++ tests/test_packet_crc_signing.py
@@ -0,0 +1,77 @@
+"""Switcher water heater packet crc signing test cases."""
+
+from binascii import hexlify
+from struct import pack
+
+from assertpy import assert_that
+from aioswitcher import consts
+from aioswitcher.utils import sign_packet_with_crc_key
+from aioswitcher.api import packets
+from aioswitcher.errors import EncodingError
+
+SUT_DEVICE_ID = "a123bc"
+SUT_SESSION_ID = "01000000"
+SUT_TIMESTAMP = "ef8db35c"
+
+
+def test_sign_packet_with_crc_key_for_a_random_string_throws_error():
+    """Test the sign_packet_with_crc_key tool with a random string unqualified as a packet."""
+    assert_that(sign_packet_with_crc_key).raises(
+        EncodingError
+    ).when_called_with("just a regular string").is_equal_to("failed to sign crc")
+
+
+def test_sign_packet_with_crc_key_for_login_packet_returns_signed_packet():
+    """Test the sign_packet_with_crc_key tool for the LOGIN_PACKET."""
+    packet = packets.LOGIN_PACKET.format(SUT_SESSION_ID, SUT_TIMESTAMP)
+    assert_that(sign_packet_with_crc_key(packet)).is_equal_to(packet + "c40a4188")
+
+
+def test_sign_packet_with_crc_key_for_get_state_packet_returns_signed_packet():
+    """Test the sign_packet_with_crc_key tool for the GET_STATE_PACKET."""
+    packet = packets.GET_STATE_PACKET.format(SUT_SESSION_ID, SUT_TIMESTAMP, SUT_DEVICE_ID)
+    assert_that(sign_packet_with_crc_key(packet)).is_equal_to(packet + "42a9a1b2")
+
+
+def test_sign_packet_with_crc_key_for_send_control_on_with_no_timer_packet_returns_signed_packet():
+    """Test the sign_packet_with_crc_key tool for the SEND_CONTROL_PACKET for on state with no timer."""
+    packet = packets.SEND_CONTROL_PACKET.format(
+        SUT_SESSION_ID, SUT_TIMESTAMP, SUT_DEVICE_ID, consts.COMMAND_ON, consts.NO_TIMER_REQUESTED)
+    assert_that(sign_packet_with_crc_key(packet)).is_equal_to(packet + "cc06bb10")
+
+
+def test_sign_packet_with_crc_key_for_send_control_off_packet_returns_signed_packet():
+    """Test the sign_packet_with_crc_key tool for the SEND_CONTROL_PACKET for off state."""
+    packet = packets.SEND_CONTROL_PACKET.format(
+        SUT_SESSION_ID, SUT_TIMESTAMP, SUT_DEVICE_ID, consts.COMMAND_OFF, consts.NO_TIMER_REQUESTED)
+    assert_that(sign_packet_with_crc_key(packet)).is_equal_to(packet + "6c432cf4")
+
+
+def test_sign_packet_with_crc_key_for_send_control_on_with_timer_packet_returns_signed_packet():
+    """Test the sign_packet_with_crc_key tool for the SEND_CONTROL_PACKET for on state with a 90 minutes timer."""
+    timer_minutes = hexlify(pack("<I", 5400)).decode()
+    packet = packets.SEND_CONTROL_PACKET.format(
+        SUT_SESSION_ID, SUT_TIMESTAMP, SUT_DEVICE_ID, consts.COMMAND_ON, timer_minutes)
+    assert_that(sign_packet_with_crc_key(packet)).is_equal_to(packet + "3b30141e")
+
+
+def test_sign_packet_with_crc_key_for_set_auto_off_packet_returns_signed_packet():
+    """Test the sign_packet_with_crc_key tool for the SET_AUTO_OFF_SET_PACKET with a 90 minutes auto-shutdown."""
+    auto_shutdown = hexlify(pack("<I", 5400)).decode()
+    packet = packets.SET_AUTO_OFF_SET_PACKET.format(SUT_SESSION_ID, SUT_TIMESTAMP, SUT_DEVICE_ID, auto_shutdown)
+    assert_that(sign_packet_with_crc_key(packet)).is_equal_to(packet + "3bb1ca55")
+
+
+def test_sign_packet_with_crc_key_for_set_device_name_packet_returns_signed_packet():
+    """Test the sign_packet_with_crc_key tool for the UPDATE_DEVICE_NAME_PACKET with a 'my device cool name'."""
+    desired_name = "my device cool name"
+    hex_name = hexlify(desired_name.encode())
+    zeros_pad = ("00" * (32 - len(desired_name))).encode()
+    packet = packets.UPDATE_DEVICE_NAME_PACKET.format(SUT_SESSION_ID, SUT_TIMESTAMP, SUT_DEVICE_ID, (hex_name + zeros_pad).decode())
+    assert_that(sign_packet_with_crc_key(packet)).is_equal_to(packet + "1039bc0e")
+
+
+def test_sign_packet_with_crc_key_for_get_schedules_packet_returns_signed_packet():
+    """Test the sign_packet_with_crc_key tool for the GET_SCHEDULES_PACKET."""
+    packet = packets.GET_SCHEDULES_PACKET.format(SUT_SESSION_ID, SUT_TIMESTAMP, SUT_DEVICE_ID)
+    assert_that(sign_packet_with_crc_key(packet)).is_equal_to(packet + "0efde536")
---
+++ tests/test_utility_functions.py
@@ -0,0 +1,159 @@
+"""Switcher water heater Utility functions test cases."""
+
+from binascii import hexlify, unhexlify
+from datetime import datetime, timedelta
+from struct import pack, unpack
+import time
+
+from assertpy import assert_that
+from aioswitcher import Weekday, utils
+from aioswitcher.errors import CalculationError, DecodingError, EncodingError
+from pytest import mark
+
+
+def test_seconds_to_iso_time_with_a_valid_seconds_value_should_return_a_time_string():
+    assert_that(utils.seconds_to_iso_time(86399)).is_equal_to("23:59:59")
+
+
+def test_seconds_to_iso_time_with_a_nagative_value_should_throw_a_calculation_error():
+    assert_that(utils.seconds_to_iso_time).raises(
+        CalculationError
+    ).when_called_with(-1).is_equal_to("failed to convert seconds to iso time")
+
+
+def test_minutes_to_hexadecimal_seconds_with_correct_minutes_should_return_expected_hex_seconds():
+    # TODO: replace the equality assertion with an unhexlified unpacked value
+    hex_sut = utils.minutes_to_hexadecimal_seconds(90)
+    assert_that(hex_sut).is_equal_to("18150000")
+
+
+def test_minutes_to_hexadecimal_seconds_with_a_negative_value_should_throw_an_encoding_error():
+    assert_that(utils.minutes_to_hexadecimal_seconds).raises(
+        EncodingError
+    ).when_called_with(-1).is_equal_to("failed to encode -1 minutes")
+
+
+def test_timedelta_to_hexadecimal_seconds_with_an_allowed_timedelta_should_return_an_hex_timestamp():
+    # TODO: replace the equality assertion with an unhexlified unpacked value
+    hex_timestamp = utils.timedelta_to_hexadecimal_seconds(timedelta(hours=1, minutes=30))
+    assert_that(hex_timestamp).is_equal_to("18150000")
+
+
+@mark.parametrize("out_of_range", [timedelta(minutes=59), timedelta(hours=24)])
+def test_timedelta_to_hexadecimal_seconds_with_an_out_of_range_value_should_throw_an_encoding_error(out_of_range):
+    assert_that(utils.timedelta_to_hexadecimal_seconds).raises(
+        EncodingError
+    ).when_called_with(out_of_range).starts_with("failed to encode")
+
+
+def test_string_to_hexadecimale_device_name_with_a_correct_length_name_should_return_a_right_zero_padded_hex_name():
+    str_name = "my device cool name"
+    hex_name = utils.string_to_hexadecimale_device_name(str_name)
+    unhexed_name = unhexlify(hex_name.rstrip("0")).decode()
+    assert_that(unhexed_name).is_equal_to(str_name)
+
+
+@mark.parametrize("unsupported_length_value", ["t", "t" * 33])
+def test_string_to_hexadecimale_device_name_with_an_unsupported_length_value_should_throw_an_encoding_error(unsupported_length_value):
+    assert_that(utils.string_to_hexadecimale_device_name).raises(
+        EncodingError
+    ).when_called_with(unsupported_length_value).starts_with("failed to encode")
+
+
+def test_current_timestamp_to_hexadecimal_should_return_the_current_timestamp():
+    hex_timestamp = utils.current_timestamp_to_hexadecimal()
+
+    binary_timestamp = unhexlify(hex_timestamp.encode())
+    unpacked_timestamp = unpack("<I", binary_timestamp)
+    sut_datetime = datetime.fromtimestamp(unpacked_timestamp[0])
+
+    assert_that(sut_datetime).is_equal_to_ignoring_time(datetime.now())
+
+
+def test_current_timestamp_to_hexadecimal_with_errornous_value_should_throw_a_decoding_error(monkeypatch):
+    # patch the time method to return a nagative value instead of the actuall timestamp
+    monkeypatch.setattr(time, "time", lambda: -1)
+
+    assert_that(utils.current_timestamp_to_hexadecimal).raises(
+        DecodingError
+    ).when_called_with().is_equal_to("failed to generate timestamp")
+
+
+def test_time_to_hexadecimal_timestamp_with_correct_time_should_return_the_expected_timestamp():
+    hex_timestamp = utils.time_to_hexadecimal_timestamp("21:00")
+
+    binary_timestamp = unhexlify(hex_timestamp.encode())
+    unpacked_timestamp = unpack("<I", binary_timestamp)
+    sut_datetime = datetime.fromtimestamp(unpacked_timestamp[0])
+    assert_that(
+        sut_datetime
+    ).is_equal_to_ignoring_time(datetime.now()).has_hour(21).has_minute(0)
+
+
+def test_time_to_hexadecimal_timestamp_with_incorrect_time_should_throw_an_encoding_error():
+    assert_that(utils.time_to_hexadecimal_timestamp).raises(
+        EncodingError
+    ).when_called_with("2100").is_equal_to("failed to encode 2100")
+
+
+def test_hexadecimale_timestamp_to_localtime_with_the_current_timestamp_should_return_a_time_string():
+    sut_datetime = datetime.now()
+    hex_timestamp = hexlify(pack("<I", round(sut_datetime.timestamp())))
+    assert_that(
+        utils.hexadecimale_timestamp_to_localtime(hex_timestamp)
+    ).is_equal_to(sut_datetime.time().strftime("%H:%M"))
+
+
+def test_hexadecimale_timestamp_to_localtime_with_wrong_value_should_throw_a_decoding_error():
+    assert_that(utils.hexadecimale_timestamp_to_localtime).raises(
+        DecodingError
+    ).when_called_with("wrongvalue".encode()).is_equal_to("failed to decode timestamp")
+
+
+@mark.parametrize("weekdays, expected_sum", [
+    (Weekday.MONDAY, 2),
+    ({Weekday.MONDAY, Weekday.TUESDAY}, 6),
+    ({Weekday.MONDAY, Weekday.TUESDAY, Weekday.WEDNESDAY}, 14),
+    ({Weekday.MONDAY, Weekday.TUESDAY, Weekday.WEDNESDAY, Weekday.THURSDAY}, 30),
+    ({Weekday.MONDAY, Weekday.TUESDAY, Weekday.WEDNESDAY, Weekday.THURSDAY, Weekday.FRIDAY}, 62),
+    ({Weekday.MONDAY, Weekday.TUESDAY, Weekday.WEDNESDAY, Weekday.THURSDAY, Weekday.FRIDAY, Weekday.SATURDAY}, 126),
+    ({Weekday.MONDAY, Weekday.TUESDAY, Weekday.WEDNESDAY, Weekday.THURSDAY, Weekday.FRIDAY, Weekday.SATURDAY, Weekday.SUNDAY}, 254),
+])
+def test_weekdays_to_hexadecimal_with_parameterized_weekday_set_should_return_the_expected_sum(weekdays, expected_sum):
+    sut_hex = utils.weekdays_to_hexadecimal(weekdays)
+    sut_int = int(sut_hex, 16)
+    assert_that(sut_int).is_equal_to(expected_sum)
+
+
+@mark.parametrize("empty_collection", [set(), (), {}, []])
+def test_weekdays_to_hexadecimal_with_empty_collections_should_throw_an_encoding_error(empty_collection):
+    assert_that(utils.weekdays_to_hexadecimal).raises(
+        EncodingError
+    ).when_called_with(empty_collection).is_equal_to("failed to calculate weekdays")
+
+
+@mark.parametrize("duplicate_members", [(Weekday.MONDAY, Weekday.MONDAY), [Weekday.MONDAY, Weekday.MONDAY]])
+def test_weekdays_to_hexadecimal_with_duplicate_members_should_throw_an_encoding_error(duplicate_members):
+    assert_that(utils.weekdays_to_hexadecimal).raises(
+        EncodingError
+    ).when_called_with(duplicate_members).is_equal_to("failed to calculate weekdays")
+
+
+@mark.parametrize("sum, expected_weekdays", [
+    (2, {Weekday.MONDAY}),
+    (6, {Weekday.MONDAY, Weekday.TUESDAY}),
+    (14, {Weekday.MONDAY, Weekday.TUESDAY, Weekday.WEDNESDAY},),
+    (30, {Weekday.MONDAY, Weekday.TUESDAY, Weekday.WEDNESDAY, Weekday.THURSDAY}),
+    (62, {Weekday.MONDAY, Weekday.TUESDAY, Weekday.WEDNESDAY, Weekday.THURSDAY, Weekday.FRIDAY}),
+    (126, {Weekday.MONDAY, Weekday.TUESDAY, Weekday.WEDNESDAY, Weekday.THURSDAY, Weekday.FRIDAY, Weekday.SATURDAY}),
+    (254, {Weekday.MONDAY, Weekday.TUESDAY, Weekday.WEDNESDAY, Weekday.THURSDAY, Weekday.FRIDAY, Weekday.SATURDAY, Weekday.SUNDAY}),
+])
+def test_bit_summary_to_weekdays_with_parameterized_sum_should_return_the_expected_weekday_set(sum, expected_weekdays):
+    assert_that(utils.bit_summary_to_weekdays(sum)).is_equal_to(expected_weekdays)
+
+
+@mark.parametrize("wrong_bit_sum", [1, 255])
+def test_bit_summary_to_weekdays_with_wrong_bit_sum_parameterized_value(wrong_bit_sum):
+    assert_that(utils.bit_summary_to_weekdays).raises(
+        DecodingError
+    ).when_called_with(wrong_bit_sum).is_equal_to("failed to decode value to weekdays")
