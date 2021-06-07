8bbd855d2b2533f9e0f121f3a74e0822b9478771
Tomer Figenblat <tomer.figenblat@gmail.com>
2021-06-07 17:30:35 +0300

    build: bumped license from mit to apache2

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,17 @@
+# Copyright Tomer Figenblat.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Configuration file for Sphinx Documentation Generator."""
 
 from os import path as os_path
@@ -11,7 +25,7 @@ toml_path = "{}/pyproject.toml".format(os_path.abspath(".."))
 parsed_toml = toml_load(toml_path)
 
 project = parsed_toml["tool"]["poetry"]["name"]
-copyright = "2019, Tomer Figenblat"
+copyright = "Tomer Figenblat"
 author = "Tomer Figenblat"
 version = parsed_toml["tool"]["poetry"]["version"]
 release = version
--- src/aioswitcher/api/packets.py
+++ src/aioswitcher/api/packets.py
@@ -1,8 +1,19 @@
-"""Switcher water heater unofficial API and bridge, API Packet formats.
+# Copyright Tomer Figenblat.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-.. codeauthor:: Tomer Figenblat <tomer.figenblat@gmail.com>
+"""Switcher smart switch unofficial API and bridge, API Packet formats."""
 
-"""
 NO_TIMER_REQUESTED = "00000000"
 # format values are local session id, timestamp
 REQUEST_FORMAT = "{}340001000000000000000000{}00000000000000000000f0fe"
