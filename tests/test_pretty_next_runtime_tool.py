642b52e84e4aa778fb0e690cc1a982b514fcf48a
Tomer Figenblat <tomer.figenblat@gmail.com>
2021-06-22 16:47:46 +0300

    test: fixed flaky test

--- tests/test_pretty_next_runtime_tool.py
+++ tests/test_pretty_next_runtime_tool.py
@@ -36,14 +36,19 @@ def todays_day(today):
     return days_by_weekdays[today.weekday()]
 
 
-def test_pretty_next_run_with_no_selected_days_should_return_due_today():
-    expected_return = "Due today at 13:00"
-    assert_that(_pretty_next_run("13:00")).is_equal_to(expected_return)
+@fixture
+def one_hour_from_now(today):
+    return datetime.strftime(today + timedelta(hours=1), "%H:%M")
 
 
-def test_pretty_next_run_with_todays_day_should_return_due_today(todays_day):
-    expected_return = "Due today at 13:00"
-    assert_that(_pretty_next_run("13:00", [todays_day])).is_equal_to(expected_return)
+def test_pretty_next_run_with_no_selected_days_should_return_due_today(one_hour_from_now):
+    expected_return = f"Due today at {one_hour_from_now}"
+    assert_that(_pretty_next_run(one_hour_from_now)).is_equal_to(expected_return)
+
+
+def test_pretty_next_run_with_todays_day_should_return_due_today(todays_day, one_hour_from_now):
+    expected_return = f"Due today at {one_hour_from_now}"
+    assert_that(_pretty_next_run(one_hour_from_now, {todays_day})).is_equal_to(expected_return)
 
 
 def test_pretty_next_run_with_multiple_days_should_return_due_the_closest_day(today):
@@ -55,25 +60,25 @@ def test_pretty_next_run_with_multiple_days_should_return_due_the_closest_day(to
 
     expected_return = f"Due next {two_days_from_now_day.value} at 13:00"
 
-    assert_that(_pretty_next_run("13:00", [four_days_from_now_day, two_days_from_now_day])).is_equal_to(expected_return)
+    assert_that(_pretty_next_run("13:00", {four_days_from_now_day, two_days_from_now_day})).is_equal_to(expected_return)
 
 
 def test_pretty_next_run_on_yesterday_with_todays_day_should_return_due_tommorow(today, todays_day):
     expected_return = "Due tommorow at 13:00"
     yesterday = today - timedelta(days=1)
     with time_machine.travel(yesterday):
-        assert_that(_pretty_next_run("13:00", [todays_day])).is_equal_to(expected_return)
+        assert_that(_pretty_next_run("13:00", {todays_day})).is_equal_to(expected_return)
 
 
 def test_pretty_next_run_on_two_days_ago_with_todays_day_should_return_due_on_next_day(today, todays_day):
     expected_return = f"Due next {todays_day.value} at 13:00"
     two_days_ago = today - timedelta(days=2)
     with time_machine.travel(two_days_ago):
-        assert_that(_pretty_next_run("13:00", [todays_day])).is_equal_to(expected_return)
+        assert_that(_pretty_next_run("13:00", {todays_day})).is_equal_to(expected_return)
 
 
 def test_pretty_next_run_on_last_sunday_with_monday_selected_should_return_due_tommorow(today):
     expected_return = "Due tommorow at 13:00"
     last_sunday = today - timedelta(days=((today.weekday() + 1) % 7))
     with time_machine.travel(last_sunday):
-        assert_that(_pretty_next_run("13:00", [Days.MONDAY])).is_equal_to(expected_return)
+        assert_that(_pretty_next_run("13:00", {Days.MONDAY})).is_equal_to(expected_return)
