2df6da74a2ec53beaf458f216f87a69db8824f2e
jafar-atili <at.jafar@outlook.com>
2022-05-30 20:01:13 +0300

    feat: add shutter and thermostats devices support
    * feat: added shutter and thermostats devices support
    
    BREAKING CHANGE: added new devices
    
    Co-Authored-By: Shay Levy <levyshay1@gmail.com>
    Signed-off-by: Jafar Atili <at.jafar@outlook.com>
    
    * chore: fixed typos
    
    Signed-off-by: Tomer Figenblat <tomer.figenblat@gmail.com>
    
    * test: fix device types and state tests
    
    Signed-off-by: thecode <levyshay1@gmail.com>
    
    * fix: fix typos
    
    Signed-off-by: thecode <levyshay1@gmail.com>
    
    Co-authored-by: Shay Levy <levyshay1@gmail.com>
    Co-authored-by: Tomer Figenblat <tomer.figenblat@gmail.com>

--- src/aioswitcher/api/packets.py
+++ src/aioswitcher/api/packets.py
@@ -21,35 +21,78 @@ NO_TIMER_REQUESTED = "00000000"
 NON_RECURRING_SCHEDULE = "00"
 # format values are local session id, timestamp
 REQUEST_FORMAT = "{}340001000000000000000000{}00000000000000000000f0fe"
-PAD_74_ZEROS = "0" * 74
+REQUEST_FORMAT2 = "{}390001000000000000000000{}00000000000000000000f0fe"
 
+REQUEST_FORMAT_BREEZE = "{}000001000000000000000000{}00000000000000000000f0fe"
+PAD_72_ZEROS = "0" * 72
 # format value just timestamp (initial session id is "00000000")
-LOGIN_PACKET = "fef052000232a10000000000" + REQUEST_FORMAT[2:] + "1c" + PAD_74_ZEROS
+LOGIN_PACKET = (
+    "fef052000232a10000000000" + REQUEST_FORMAT[2:] + "1c" + PAD_72_ZEROS + "00"
+)
+
+LOGIN2_PACKET = (
+    "fef030000305a60000000000ff0301000000000000000000{}00000000000000000000f0fe{}00"
+)
+
 # format values are local session id, timestamp, device id
 GET_STATE_PACKET = "fef0300002320103" + REQUEST_FORMAT + "{}00"
+
+# format values are local session id, timestamp, device id
+GET_STATE_PACKET2 = "fef0300003050103" + REQUEST_FORMAT2 + "{}00"
+
 # format values are local session id, timestamp, device id, command, timer
 SEND_CONTROL_PACKET = (
-    "fef05d0002320102" + REQUEST_FORMAT + "{}" + PAD_74_ZEROS + "0106000{}00{}"
+    "fef05d0002320102" + REQUEST_FORMAT + "{}" + PAD_72_ZEROS + "000106000{}00{}"
 )
 # format values are local session id, timestamp, device id, auto-off seconds
 SET_AUTO_OFF_SET_PACKET = (
-    "fef05b0002320102" + REQUEST_FORMAT + "{}" + PAD_74_ZEROS + "040400{}"
+    "fef05b0002320102" + REQUEST_FORMAT + "{}" + PAD_72_ZEROS + "00040400{}"
 )
 # format values are local session id, timestamp, device id, name
 UPDATE_DEVICE_NAME_PACKET = (
-    "fef0740002320202" + REQUEST_FORMAT + "{}" + PAD_74_ZEROS + "{}"
+    "fef0740002320202" + REQUEST_FORMAT + "{}" + PAD_72_ZEROS + "00{}"
 )
 # format values are local session id, timestamp, device id
 GET_SCHEDULES_PACKET = (
-    "fef0570002320102" + REQUEST_FORMAT + "{}" + PAD_74_ZEROS + "060000"
+    "fef0570002320102" + REQUEST_FORMAT + "{}" + PAD_72_ZEROS + "00060000"
 )
 # format values are local session id, timestamp, device id, schedule id
 DELETE_SCHEDULE_PACKET = (
-    "fef0580002320102" + REQUEST_FORMAT + "{}" + PAD_74_ZEROS + "0801000{}"
+    "fef0580002320102" + REQUEST_FORMAT + "{}" + PAD_72_ZEROS + "000801000{}"
 )
 # format values are local session id, timestamp, device id,
 # schedule data =
 #                   (on_off + week + timstate + start_time + end_time)
 CREATE_SCHEDULE_PACKET = (
-    "fef0630002320102" + REQUEST_FORMAT + "{}" + PAD_74_ZEROS + "030c00ff{}"
+    "fef0630002320102" + REQUEST_FORMAT + "{}" + PAD_72_ZEROS + "00030c00ff{}"
+)
+# format values are local session id, timestamp, device id, phone id, device-
+# passwored, command length, command
+BREEZE_COMMAND_PACKET = (
+    "fef0000003050102" + REQUEST_FORMAT_BREEZE + "{}" + PAD_72_ZEROS + "3701" + "{}{}"
+)
+
+# format values are local session id, timestamp, device id
+RUNNER_STOP_COMMAND = (
+    "fef0590003050102"
+    + "{}"
+    + "232301"
+    + "000000000000000000"
+    + "{}"
+    + "00000000000000000000f0fe"
+    + "{}"
+    + PAD_72_ZEROS
+    + "370202000000"
+)
+
+RUNNER_SET_POSITION = (
+    "fef0580003050102{}290401"
+    + "000000000000000000"
+    + "{}"
+    + "00000000000000000000f0fe"
+    + "{}"
+    + PAD_72_ZEROS
+    + "3701"
+    + "0100"
+    + "{}"
 )
--- src/aioswitcher/bridge.py
+++ src/aioswitcher/bridge.py
@@ -29,14 +29,31 @@ from .device import (
     DeviceCategory,
     DeviceState,
     DeviceType,
+    ShutterDirection,
     SwitcherBase,
     SwitcherPowerPlug,
+    SwitcherShutter,
+    SwitcherThermostat,
     SwitcherWaterHeater,
+    ThermostatFanLevel,
+    ThermostatMode,
+    ThermostatSwing,
 )
 from .device.tools import seconds_to_iso_time, watts_to_amps
 
 __all__ = ["SwitcherBridge"]
 logger = getLogger(__name__)
+
+
+SWITCHER_UDP_PORT = 20002
+SWITCHER_UDP_PORT2 = 20003
+
+SWITCHER_DEVICE_TO_UDP_PORT = {
+    DeviceCategory.WATER_HEATER: SWITCHER_UDP_PORT,
+    DeviceCategory.POWER_PLUG: SWITCHER_UDP_PORT,
+    DeviceCategory.THERMOSTAT: SWITCHER_UDP_PORT2,
+    DeviceCategory.SHUTTER: SWITCHER_UDP_PORT2,
+}
 
 
 def _parse_device_from_datagram(
@@ -55,8 +72,11 @@ def _parse_device_from_datagram(
     if not parser.is_switcher_originator():
         logger.debug("received datagram from an unknown source")
     else:
-        device_type = parser.get_device_type()
-        device_state = parser.get_device_state()
+        device_type: DeviceType = parser.get_device_type()
+        if device_type == DeviceType.BREEZE:
+            device_state = parser.get_thermostat_state()
+        else:
+            device_state = parser.get_device_state()
         if device_state == DeviceState.ON:
             power_consumption = parser.get_power_consumption()
             electric_current = watts_to_amps(power_consumption)
@@ -97,6 +117,40 @@ def _parse_device_from_datagram(
                     parser.get_name(),
                     power_consumption,
                     electric_current,
+                )
+            )
+
+        elif device_type and device_type.category == DeviceCategory.SHUTTER:
+            logger.debug("discovered a Runner switch switcher device")
+            device_callback(
+                SwitcherShutter(
+                    device_type,
+                    DeviceState.ON,
+                    parser.get_device_id(),
+                    parser.get_ip2(),
+                    parser.get_mac(),
+                    parser.get_name(),
+                    parser.get_shutter_position(),
+                    parser.get_shutter_direction(),
+                )
+            )
+
+        elif device_type and device_type.category == DeviceCategory.THERMOSTAT:
+            logger.debug("discovered a Breeze switcher device")
+            device_callback(
+                SwitcherThermostat(
+                    device_type,
+                    device_state,
+                    parser.get_device_id(),
+                    parser.get_ip2(),
+                    parser.get_mac(),
+                    parser.get_name(),
+                    parser.get_thermostat_mode(),
+                    parser.get_thermostat_temp(),
+                    parser.get_thermostat_target_temp(),
+                    parser.get_thermostat_fan_level(),
+                    parser.get_thermostat_swing(),
+                    parser.get_thermostat_remote_id(),
                 )
             )
         else:
@@ -208,8 +262,10 @@ class DatagramParser:
 
     def is_switcher_originator(self) -> bool:
         """Verify the broadcast message had originated from a switcher device."""
-        return (
-            hexlify(self.message)[0:4].decode() == "fef0" and len(self.message) == 165
+        return hexlify(self.message)[0:4].decode() == "fef0" and (
+            len(self.message) == 165
+            or len(self.message) == 168  # Switcher Breeze
+            or len(self.message) == 159  # Switcher Runner and RunnerMini
         )
 
     def get_ip(self) -> str:
@@ -217,6 +273,12 @@ class DatagramParser:
         hex_ip = hexlify(self.message)[152:160]
         ip_addr = int(hex_ip[6:8] + hex_ip[4:6] + hex_ip[2:4] + hex_ip[0:2], 16)
         return inet_ntoa(pack("<L", ip_addr))
+
+    def get_ip2(self) -> str:
+        """Extract the IP address from the broadcast message (Breeze and Runners)."""
+        hex_ip = hexlify(self.message)[154:162]
+        ip_addr = int(hex_ip[0:2] + hex_ip[2:4] + hex_ip[4:6] + hex_ip[6:8], 16)
+        return inet_ntoa(pack(">L", ip_addr))
 
     def get_mac(self) -> str:
         """Extract the MAC address from the broadcast message."""
@@ -245,7 +307,7 @@ class DatagramParser:
 
     def get_device_state(self) -> DeviceState:
         """Extract the device state from the broadcast message."""
-        hex_device_state = hexlify(self.message)[266:270].decode()
+        hex_device_state = hexlify(self.message)[266:268].decode()
         return (
             DeviceState.ON
             if hex_device_state == DeviceState.ON.value
@@ -283,6 +345,62 @@ class DatagramParser:
 
     def get_device_type(self) -> DeviceType:
         """Extract the device type from the broadcast message."""
-        hex_model = hexlify(self.message[75:76]).decode()
+        hex_model = hexlify(self.message[74:76]).decode()
         devices = dict(map(lambda d: (d.hex_rep, d), DeviceType))
         return devices[hex_model]
+
+    # Switcher Runner and Runner Mini methods
+
+    def get_shutter_position(self) -> int:
+        """Return the current position of the shutter 0 <= pos <= 100."""
+        hex_pos = hexlify(self.message[135:137]).decode()
+        return int(hex_pos[2:4]) + int(hex_pos[0:2], 16)
+
+    def get_shutter_direction(self) -> ShutterDirection:
+        """Return the current direction of the shutter (UP/DOWN/STOP)."""
+        hex_direction = hexlify(self.message[137:139]).decode()
+        directions = dict(map(lambda d: (d.value, d), ShutterDirection))
+        return directions[hex_direction]
+
+    # Switcher Breeze methods
+
+    def get_thermostat_temp(self) -> float:
+        """Return the current temp of the thermostat."""
+        hex_temp = hexlify(self.message[135:137]).decode()
+        return int(hex_temp[2:4] + hex_temp[0:2], 16) / 10
+
+    def get_thermostat_state(self) -> DeviceState:
+        """Return the current thermostat state."""
+        hex_power = hexlify(self.message[137:138]).decode()
+        return DeviceState.ON if hex_power == DeviceState.ON.value else DeviceState.OFF
+
+    def get_thermostat_mode(self) -> ThermostatMode:
+        """Return the current thermostat mode."""
+        hex_mode = hexlify(self.message[138:139]).decode()
+        states = dict(map(lambda s: (s.value, s), ThermostatMode))
+        return ThermostatMode.COOL if hex_mode not in states else states[hex_mode]
+
+    def get_thermostat_target_temp(self) -> int:
+        """Return the current temp of the thermostat."""
+        hex_temp = hexlify(self.message[139:140]).decode()
+        return int(hex_temp, 16)
+
+    def get_thermostat_fan_level(self) -> ThermostatFanLevel:
+        """Return the current thermostat fan level."""
+        hex_level = hexlify(self.message[140:141]).decode()
+        states = dict(map(lambda s: (s.value, s), ThermostatFanLevel))
+        return states[hex_level[0:1]]
+
+    def get_thermostat_swing(self) -> ThermostatSwing:
+        """Return the current thermostat fan swing."""
+        hex_swing = hexlify(self.message[140:141]).decode()
+
+        return (
+            ThermostatSwing.OFF
+            if hex_swing[1:2] == ThermostatSwing.OFF.value
+            else ThermostatSwing.ON
+        )
+
+    def get_thermostat_remote_id(self) -> str:
+        """Return the current thermostat remote."""
+        return self.message[143:151].decode()
