20a819e187404717dd181ecbc77a06c82ff15540
thecode <levyshay1@gmail.com>
2022-05-31 16:56:51 +0300

    feat: build protocol type help dynamically
    Signed-off-by: thecode <levyshay1@gmail.com>

--- tests/test_udp_datagram_parsing.py
+++ tests/test_udp_datagram_parsing.py
@@ -24,24 +24,16 @@ from aioswitcher.bridge import DatagramParser
 from aioswitcher.device import DeviceState, DeviceType
 
 
-@mark.parametrize(
-    "type_suffix, expected_type",
-    [
-        ("mini", DeviceType.MINI),
-        ("power_plug", DeviceType.POWER_PLUG),
-        ("touch", DeviceType.TOUCH),
-        ("v2_esp", DeviceType.V2_ESP),
-        ("v2_qca", DeviceType.V2_QCA),
-        ("v4", DeviceType.V4),
-    ],
-)
+@mark.parametrize("type_suffix, expected_type", [
+    ("mini", DeviceType.MINI),
+    ("power_plug", DeviceType.POWER_PLUG),
+    ("touch", DeviceType.TOUCH),
+    ("v2_esp", DeviceType.V2_ESP),
+    ("v2_qca", DeviceType.V2_QCA),
+    ("v4", DeviceType.V4),
+])
 def test_datagram_state_off(resource_path, type_suffix, expected_type):
-    sut_datagram = (
-        Path(f"{resource_path}_{type_suffix}.txt")
-        .read_text()
-        .replace("\n", "")
-        .encode()
-    )
+    sut_datagram = Path(f'{resource_path}_{type_suffix}.txt').read_text().replace('\n', '').encode()
 
     sut_parser = DatagramParser(unhexlify(sut_datagram))
 
@@ -58,24 +50,16 @@ def test_datagram_state_off(resource_path, type_suffix, expected_type):
         assert_that(sut_parser.get_auto_shutdown()).is_equal_to("03:00:00")
 
 
-@mark.parametrize(
-    "type_suffix, expected_type",
-    [
-        ("mini", DeviceType.MINI),
-        ("power_plug", DeviceType.POWER_PLUG),
-        ("touch", DeviceType.TOUCH),
-        ("v2_esp", DeviceType.V2_ESP),
-        ("v2_qca", DeviceType.V2_QCA),
-        ("v4", DeviceType.V4),
-    ],
-)
+@mark.parametrize("type_suffix, expected_type", [
+    ("mini", DeviceType.MINI),
+    ("power_plug", DeviceType.POWER_PLUG),
+    ("touch", DeviceType.TOUCH),
+    ("v2_esp", DeviceType.V2_ESP),
+    ("v2_qca", DeviceType.V2_QCA),
+    ("v4", DeviceType.V4),
+])
 def test_datagram_state_on(resource_path, type_suffix, expected_type):
-    sut_datagram = (
-        Path(f"{resource_path}_{type_suffix}.txt")
-        .read_text()
-        .replace("\n", "")
-        .encode()
-    )
+    sut_datagram = Path(f'{resource_path}_{type_suffix}.txt').read_text().replace('\n', '').encode()
 
     sut_parser = DatagramParser(unhexlify(sut_datagram))
 
@@ -94,11 +78,6 @@ def test_datagram_state_on(resource_path, type_suffix, expected_type):
 
 @mark.parametrize("type_suffix", ["too_short", "wrong_start"])
 def test_a_faulty_datagram(resource_path, type_suffix):
-    sut_datagram = (
-        Path(f"{resource_path}_{type_suffix}.txt")
-        .read_text()
-        .replace("\n", "")
-        .encode()
-    )
+    sut_datagram = Path(f'{resource_path}_{type_suffix}.txt').read_text().replace('\n', '').encode()
     sut_parser = DatagramParser(unhexlify(sut_datagram))
     assert_that(sut_parser.is_switcher_originator()).is_false()
