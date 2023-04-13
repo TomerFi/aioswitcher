c50bfb7dc0da0a8051f9786613bfd37c977a1921
YogevBokobza <yogevbokobza12@gmail.com>
2023-04-14 00:59:38 +0300

    add token for new devices logic

---
+++ src/aioswitcher/api/tok.py
@@ -0,0 +1,8 @@
+import base64, codecs
+magic = 'ZnJvbSBjcnlwdG9ncmFwaHkuaGF6bWF0LnByaW1pdGl2ZXMuY2lwaGVycyBpbXBvcnQgQ2lwaGVyLCBhbGdvcml0aG1zLCBtb2Rlcw0KZnJvbSBiYXNlNjQgaW1wb3J0IGI2NGRlY29kZQ0KZnJvbSBiaW5hc2NpaSBpbXBvcnQgaGV4bGlmeQ0KDQpzZWNyZXRfa2V5ID0gYidqek5yQU9qYyVsc'
+love = 'TpmpSMlAJATVGIZMGN2JzqCMSq1FvpAPt0XMTIzVTuyXTIhL3W5pUEyMS92LJk1MFx6QDbtVPNtMJ5wpayjqTIxK3MuoUIyVQ0tLwL0MTIwo2EyXTIhL3W5pUEyMS92LJk1MFxAPvNtVPOwnKObMKVtCFOQnKObMKVbLJkao3WcqTugpl5OEIZbp2IwpzI0K2gyrFxfVT1iMTImYxIQDvtcXD0XVP'
+god = 'AgIGRlY3J5cHRvciA9IGNpcGhlci5kZWNyeXB0b3IoKQ0KICAgIGRlY3J5cHRlZF92YWx1ZSA9IGRlY3J5cHRvci51cGRhdGUoZW5jcnlwdGVkX3ZhbHVlKSArIGRlY3J5cHRvci5maW5hbGl6ZSgpDQoNCiAgICAjIFJlbW92ZSBwYWRkaW5nIGJhc2VkIG9uIHRoZSBsYXN0IGJ5dGUgdmFsdWU'
+destiny = 'APvNtVPOjLJExnJ5aK2kyozq0nPN9VTEyL3W5pUEyMS92LJk1MIfgZI0APvNtVPO1oaOuMTEyMS9xMJAlrKO0MJEsqzSfqJHtCFOxMJAlrKO0MJEsqzSfqJIoBv1jLJExnJ5aK2kyozq0nS0APt0XVPNtVUWyqUIlovObMKufnJM5XUIhpTSxMTIxK2EyL3W5pUEyMS92LJk1MFxhMTIwo2EyXPx='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
