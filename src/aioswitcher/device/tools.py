4361629cf578da203bf63ec617017e84fa7f1a47
YogevBokobza <yogevbokobza12@gmail.com>
2025-02-18 22:15:13 +0200

    fix for edge case where hours can be higher than 24 in old firmware

--- src/aioswitcher/device/tools.py
+++ src/aioswitcher/device/tools.py
@@ -44,6 +44,8 @@ def seconds_to_iso_time(all_seconds: int) -> str:
     """
     minutes, seconds = divmod(int(all_seconds), 60)
     hours, minutes = divmod(minutes, 60)
+    if hours > 24:
+        hours = 0
 
     return datetime.time(hour=hours, minute=minutes, second=seconds).isoformat()
 
