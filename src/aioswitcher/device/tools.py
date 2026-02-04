c58a06787dd285f4ed217b424f6843680d70d8af
YogevBokobza <yogevbokobza12@gmail.com>
2026-02-04 14:37:05 +0200

    Fix tools.py
    Signed-off-by: YogevBokobza <yogevbokobza12@gmail.com>

--- src/aioswitcher/device/tools.py
+++ src/aioswitcher/device/tools.py
@@ -175,6 +175,7 @@ async def validate_token(username: str, token: str) -> bool:
     is_token_valid = False
     # Preload the SSL context
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    ssl_context.load_default_certs()
 
     logger.debug("calling API call for Switcher to validate the token")
 
